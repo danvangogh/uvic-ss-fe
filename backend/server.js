02c76a884534e9057bebbbf95b92497ff8c463c9
Daniel White <danielredwhite@gmail.com>
2024-10-01 17:27:04 -0700

    dist up folder

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const path = require("path");
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "content-prism/dist")));
+app.use(express.static(path.join(__dirname, "../content-prism/dist")));
 
 const MAKE_WEBHOOK_SCRAPEURLADDRECORD =
   process.env.MAKE_WEBHOOK_SCRAPEURLADDRECORD;
