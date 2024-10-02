1f0a38780c44d33b94592e56d0dad50adc7c688c
Daniel White <danielredwhite@gmail.com>
2024-10-01 22:21:50 -0700

    page title

--- content-prism/src/router/index.js
+++ content-prism/src/router/index.js
@@ -1,32 +1,41 @@
 import { createRouter, createWebHistory } from "vue-router";
-// import HomeView from "../views/HomeView.vue";
+import HomeView from "../views/HomeView.vue";
+import ContentRequest from "../views/ContentRequest.vue";
+import Dashboard from "../views/Dashboard.vue";
+import RecordDetail from "../views/RecordDetail.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
-    component: () => import("../views/HomeView.vue"),
+    component: HomeView,
   },
   {
     path: "/content-request",
     name: "ContentRequestView",
-    component: () => import("../views/ContentRequest.vue"),
+    component: ContentRequest,
   },
   {
     path: "/dashboard",
     name: "dashboard",
-    component: () => import("../views/Dashboard.vue"),
+    component: Dashboard,
   },
   {
     path: "/record/:id",
     name: "recordDetail",
-    component: () => import("../views/RecordDetail.vue"),
+    component: RecordDetail,
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+// Navigation guard to update the document title
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title || "Prism of Content";
+  next();
 });
 
 export default router;
