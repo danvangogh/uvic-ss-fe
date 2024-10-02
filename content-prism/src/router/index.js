b7eea9698282c6010b2dcbf95e01df9927d73c7e
Daniel White <danielredwhite@gmail.com>
2024-10-01 21:43:44 -0700

    home welcome message

--- content-prism/src/router/index.js
+++ content-prism/src/router/index.js
@@ -2,21 +2,16 @@ import { createRouter, createWebHistory } from "vue-router";
 // import HomeView from "../views/HomeView.vue";
 
 const routes = [
-  // {
-  //   path: "/",
-  //   name: "home",
-  //   component: HomeView,
-  // },
+  {
+    path: "/",
+    name: "home",
+    component: () => import("../views/HomeView.vue"),
+  },
   {
     path: "/content-request",
     name: "ContentRequestView",
     component: () => import("../views/ContentRequest.vue"),
   },
-  // {
-  //   path: "/about",
-  //   name: "about",
-  //   component: () => import("../views/AboutView.vue"),
-  // },
   {
     path: "/dashboard",
     name: "dashboard",
---
+++ content-prism/src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home-container">
+    <h1>Welcome to the Prism of Content</h1>
+    <p>Make a content request to get started.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+};
+</script>
+
+<style scoped>
+.home-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start; /* Align content towards the top */
+  align-items: center;
+  height: 100vh;
+  text-align: center;
+  padding-top: 20vh; /* Add padding to position the content lower */
+}
+
+h1 {
+  font-size: 2.5em;
+  margin-bottom: 0.5em;
+}
+
+p {
+  font-size: 1.2em;
+}
+</style>
